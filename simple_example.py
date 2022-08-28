2c2d4481ad403a0375da47b90589f81268257c80
Antonyo314 <makarenko.antonf2c@gmail.com>
2022-08-28 16:10:00 +0300

    add simple example of using environment

---
+++ simple_example.py
@@ -0,0 +1,16 @@
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = JoypadSpace(env, COMPLEX_MOVEMENT)
+
+done = True
+
+while True:
+    if done:
+        state = env.reset()
+    state, reward, done, info = env.step(env.action_space.sample())
+    env.render()
+
+env.close()
