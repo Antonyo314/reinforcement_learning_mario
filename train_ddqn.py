ad0cd1ce29b22a359a9426cd3d7859d217f527df
Antonyo314 <makarenko.antonf2c@gmail.com>
2022-09-13 13:55:32 +0300

    add additional levels support

--- train_ddqn.py
+++ train_ddqn.py
@@ -14,24 +14,6 @@ from torch import nn
 
 from wrappers import *
 
-env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
-env = JoypadSpace(env, [["right"], ["right", "A"]])
-
-# seed everything
-# TODO looks like  epsilon greedy and env.random() needs additional seeds
-env.seed(0)
-env.action_space.seed(0)
-torch.manual_seed(0)
-torch.random.manual_seed(0)
-random.seed(0)
-np.random.seed(0)
-
-# apply wrappers
-env = SkipFrame(env, skip=4)
-env = GrayScaleObservation(env)
-env = ResizeObservation(env, shape=84)
-env = FrameStack(env, num_stack=4)
-
 
 class MarioNet(nn.Module):
     def __init__(self, state_dim, output_dim):
@@ -65,14 +47,14 @@ class MarioNet(nn.Module):
 
 
 class Agent:
-    def __init__(self, action_space_dim):
+    def __init__(self, action_space_dim, level):
         self.action_space_dim = action_space_dim
         if device == 'cuda':
             self.net = MarioNet(state_dim=(4, 84, 84), output_dim=self.action_space_dim).cuda()
         else:
             self.net = MarioNet(state_dim=(4, 84, 84), output_dim=self.action_space_dim)
 
-        self.save_dir = datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+        self.save_dir = datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')+f'_{level}'
         self.exploration_rate = 1.0
         self.exploration_rate_decay = 0.99
         self.exploration_rate_min = 0.01
@@ -177,13 +159,34 @@ class Agent:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--render', action='store_true')
+    parser.add_argument('--level', type=str, default='SuperMarioBros-1-1-v0')
     args = parser.parse_args()
     render = args.render
+    level = args.level
+
+    env = gym_super_mario_bros.make(level)
+    env = JoypadSpace(env, [["right"], ["right", "A"]])
+
+    # seed everything
+    # TODO looks like  epsilon greedy and env.random() needs additional seeds
+    env.seed(0)
+    env.action_space.seed(0)
+    torch.manual_seed(0)
+    torch.random.manual_seed(0)
+    random.seed(0)
+    np.random.seed(0)
+
+    # apply wrappers
+    env = SkipFrame(env, skip=4)
+    env = GrayScaleObservation(env)
+    env = ResizeObservation(env, shape=84)
+    env = FrameStack(env, num_stack=4)
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     checkpoint_save_period = 10
     log_period_ = 1
-    agent = Agent(action_space_dim=env.action_space.n)
+    agent = Agent(action_space_dim=env.action_space.n, level=level)
 
     episode = 0
     while True:
